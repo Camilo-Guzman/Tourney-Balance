afab2b625a90733ed575957ddff06779fcfe69c5
Camilo-Guzman <camilo_guzman1997@hotmail.com>
2022-07-31 15:48:41 +0200

    Waystalker Update

--- scripts/mods/TourneyBalance/vermintide-mod-builder/.template-vmf/scripts/mods/%%name/%%name.lua
+++
@@ -1,4 +0,0 @@
-local mod = get_mod("%%name")
-
--- Your mod code goes here.
--- https://vmf-docs.verminti.de
--- scripts/mods/TourneyBalance/vermintide-mod-builder/.template-vmf/scripts/mods/%%name/%%name_data.lua
+++
@@ -1,7 +0,0 @@
-local mod = get_mod("%%name")
-
-return {
-	name = "%%title",
-	description = mod:localize("mod_description"),
-	is_togglable = true,
-}
