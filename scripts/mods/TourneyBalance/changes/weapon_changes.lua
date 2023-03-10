ae151eb8b6b8a89eeb763db9e13c6dff624bd35c
Camilo-Guzman <camilo_guzman1997@hotmail.com>
2023-03-10 20:50:15 +0100

    Axes

--- scripts/mods/TourneyBalance/changes/weapon_changes.lua
+++ scripts/mods/TourneyBalance/changes/weapon_changes.lua
@@ -1608,6 +1608,102 @@ NewDamageProfileTemplates.tb_two_handed_sword_light = {
 	}
 }
 
+--1h Axe
+Weapons.one_hand_axe_template_1.actions.action_one.light_attack_last.anim_time_scale = 1.3 --1.035
+Weapons.one_hand_axe_template_2.actions.action_one.light_attack_last.anim_time_scale = 1.3 --1.035
+Weapons.one_hand_axe_template_1.actions.action_one.light_attack_last.damage_profile = "light_1h_axe_tb"
+Weapons.one_hand_axe_template_2.actions.action_one.light_attack_last.damage_profile = "light_1h_axe_tb"
+Weapons.one_hand_axe_template_1.actions.action_one.light_attack_left.damage_profile = "light_1h_axe_tb"
+Weapons.one_hand_axe_template_2.actions.action_one.light_attack_left.damage_profile = "light_1h_axe_tb"
+Weapons.one_hand_axe_template_1.actions.action_one.light_attack_right.damage_profile = "light_1h_axe_tb"
+Weapons.one_hand_axe_template_2.actions.action_one.light_attack_right.damage_profile = "light_1h_axe_tb"
+Weapons.one_hand_axe_template_1.actions.action_one.light_attack_bopp.damage_profile = "light_1h_axe_tb"
+Weapons.one_hand_axe_template_2.actions.action_one.light_attack_bopp.damage_profile = "light_1h_axe_tb"
+
+--Heavy
+Weapons.one_hand_axe_template_1.actions.action_one.heavy_attack_left.range_mod = 1.2 --1
+Weapons.one_hand_axe_template_1.actions.action_one.heavy_attack_right.range_mod = 1.2 --1
+Weapons.one_hand_axe_template_2.actions.action_one.heavy_attack_left.range_mod = 1.2 --1
+Weapons.one_hand_axe_template_2.actions.action_one.heavy_attack_right.range_mod = 1.2 --1
+
+NewDamageProfileTemplates.light_1h_axe_tb = {
+	armor_modifier = {
+		attack = {
+			1.25,
+			0.65,
+			2.5,
+			1,
+			0.75,
+			0.6
+		},
+		impact = {
+			1,
+			0.5,
+			1,
+			1,
+			0.75,
+			0.25
+		}
+	},
+	critical_strike = {
+		attack_armor_power_modifer = {
+			1.25,
+			0.75,
+			2.75,
+			1,
+			1
+		},
+		impact_armor_power_modifer = {
+			1,
+			1,
+			1,
+			1,
+			1
+		}
+	},
+	charge_value = "light_attack",
+	cleave_distribution = {
+		attack = 0.075,
+		impact = 0.075
+	},
+	default_target = {
+		boost_curve_type = "smiter_curve",
+		attack_template = "slashing_smiter",
+		boost_curve_coefficient_headshot = 2,
+		power_distribution = {
+			attack = 0.25,
+			impact = 0.175
+		}
+	},
+	ignore_stagger_reduction = true,
+	targets =  {
+		[2] = {
+			boost_curve_type = "smiter_curve",
+			attack_template = "slashing_smiter",
+			armor_modifier = {
+				attack = {
+					1,
+					0.25,
+					1,
+					1,
+					0.75
+				},
+				impact = {
+					0.75,
+					0.25,
+					1,
+					1,
+					0.75
+				}
+			},
+			power_distribution = {
+				attack = 0.075,
+				impact = 0.075
+			}
+		}
+	},
+}
+
 --2h Axe
 Weapons.two_handed_axes_template_1.actions.action_one.heavy_attack_right.damage_profile = "heavy_2h_axe_tb"
 Weapons.two_handed_axes_template_1.actions.action_one.heavy_attack_left.damage_profile = "heavy_2h_axe_tb"
@@ -1616,6 +1712,8 @@ Weapons.two_handed_axes_template_1.actions.action_one.heavy_attack_left.slide_ar
 Weapons.two_handed_axes_template_1.actions.action_one.heavy_attack_right.hit_mass_count = HEAVY_LINESMAN_HIT_MASS_COUNT --nil
 Weapons.two_handed_axes_template_1.actions.action_one.heavy_attack_left.hit_mass_count = HEAVY_LINESMAN_HIT_MASS_COUNT --nil
 Weapons.two_handed_axes_template_1.actions.action_one.light_attack_up.anim_time_scale = 0.9 --0.81
+Weapons.two_handed_axes_template_1.actions.action_one.light_attack_bopp.additional_critical_strike_chance = 0.1
+Weapons.two_handed_axes_template_1.actions.action_one.light_attack_bopp.damage_profile = "bopp_2h_axe_tb"
 NewDamageProfileTemplates.heavy_2h_axe_tb = {
 	armor_modifier = {
 		attack = {
@@ -1638,7 +1736,7 @@ NewDamageProfileTemplates.heavy_2h_axe_tb = {
 	critical_strike = {
 		attack_armor_power_modifer = {
 			1,
-			0.75,
+			0.6,
 			2,
 			1,
 			1,
@@ -1679,7 +1777,7 @@ NewDamageProfileTemplates.heavy_2h_axe_tb = {
 			armor_modifier = {
 				attack = {
 					0.9,
-					0.75,
+					0.5,
 					2,
 					1,
 					0.75
@@ -1709,6 +1807,97 @@ NewDamageProfileTemplates.heavy_2h_axe_tb = {
 			power_distribution = {
 				attack = 0.25,
 				impact = 0.125
+			}
+		}
+	}
+}
+
+NewDamageProfileTemplates.bopp_2h_axe_tb = {
+	armor_modifier = {
+		attack = {
+			0.9,
+			0,
+			1.5,
+			1,
+			0.75
+		},
+		impact = {
+			0.9,
+			0.5,
+			1,
+			1,
+			0.75
+		}
+	},
+	critical_strike = {
+		attack_armor_power_modifer = {
+			1,
+			0.5,
+			1.5,
+			1,
+			1
+		},
+		impact_armor_power_modifer = {
+			1,
+			1,
+			1,
+			1,
+			1
+		}
+	},
+	charge_value = "light_attack",
+	cleave_distribution = {
+		attack = 0.275,
+		impact = 0.6
+	},
+	default_target = {
+		boost_curve_type = "linesman_curve",
+		attack_template = "light_slashing_linesman",
+		power_distribution = {
+			attack = 0.075,
+			impact = 0.075
+		}
+	},
+	targets = {
+		{
+			boost_curve_coefficient_headshot = 1.5,
+			boost_curve_type = "linesman_curve",
+			attack_template = "heavy_slashing_linesman",
+			power_distribution = {
+				attack = 0.25,
+				impact = 0.2
+			},
+			armor_modifier = {
+				attack = {
+					1,
+					0.3,
+					1.5,
+					1,
+					0.75
+				},
+				impact = {
+					0.9,
+					0.75,
+					1,
+					1,
+					0.75
+				}
+			}
+		},
+		{
+			boost_curve_type = "linesman_curve",
+			attack_template = "slashing_linesman",
+			power_distribution = {
+				attack = 0.15,
+				impact = 0.125
+			}
+		},
+		{
+			boost_curve_type = "linesman_curve",
+			attack_template = "light_slashing_linesman",
+			power_distribution = {
+				attack = 0.1,
+				impact = 0.1
 			}
 		}
 	}
