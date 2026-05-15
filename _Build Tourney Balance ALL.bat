aeb667f102ae495963b91eb0d9530ed72ba5da06
Leonhard Chen <chen.leonhard@gmail.com>
2026-05-15 23:25:45 +0200

    New Upload Pipeline Test

---
+++ _Build Tourney Balance ALL.bat
@@ -0,0 +1,56 @@
+@echo off
+echo VERMINTIDE MOD BUILDER (OFFICIAL & OPEN BETA)
+echo #############################################
+echo. 
+
+echo [System] Renaming folder to TourneyBalance...
+ren "mods\Tourney-Balance-Open-Beta" "TourneyBalance"
+
+:: Set a base path variable to keep the code clean
+set "MOD_PATH=mods\TourneyBalance"
+
+echo [System] Cleaning up old build folders...
+if exist "%MOD_PATH%\bundleV2" rmdir /s /q "%MOD_PATH%\bundleV2"
+if exist "%MOD_PATH%\bundleV2-Official" rmdir /s /q "%MOD_PATH%\bundleV2-Official"
+if exist "%MOD_PATH%\bundleV2-Open-Beta" rmdir /s /q "%MOD_PATH%\bundleV2-Open-Beta"
+echo.
+
+echo ==================================================
+echo STEP 1: Building "Tourney Balance Official"
+echo ==================================================
+echo. 
+vmb build "TourneyBalance" -g 2
+echo.
+
+echo [System] Isolating Official build outputs...
+ren "%MOD_PATH%\bundleV2" "bundleV2-Official"
+ren "%MOD_PATH%\itemV2.cfg" "itemV2-Official.cfg"
+
+echo [System] Preparing Open Beta configuration...
+ren "%MOD_PATH%\itemV2-Open-Beta.cfg" "itemV2.cfg"
+echo.
+
+echo ==================================================
+echo STEP 2: Building "Tourney Balance Open Beta"
+echo ==================================================
+echo. 
+vmb build "TourneyBalance" -g 2
+echo.
+
+echo [System] Isolating Open Beta build outputs...
+ren "%MOD_PATH%\bundleV2" "bundleV2-Open-Beta"
+echo.
+
+echo ==================================================
+echo STEP 3: Restoring files and folder names...
+echo ==================================================
+ren "%MOD_PATH%\itemV2.cfg" "itemV2-Open-Beta.cfg"
+ren "%MOD_PATH%\itemV2-Official.cfg" "itemV2.cfg"
+ren "%MOD_PATH%\bundleV2-Official" "bundleV2"
+
+echo [System] Restoring root folder name to Tourney-Balance-Open-Beta...
+ren "mods\TourneyBalance" "Tourney-Balance-Open-Beta"
+echo.
+
+echo Done! Both variants have been built successfully.
+pause
