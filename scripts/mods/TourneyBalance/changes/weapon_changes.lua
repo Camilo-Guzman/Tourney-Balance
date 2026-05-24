00a500f603c7e4c354d304ff12bb4e9631141f8f
SlashKex <slashkex0@gmail.com>
2026-05-24 15:26:07 +0100

    Energy Recharge Rate Nerf

--- scripts/mods/TourneyBalance/changes/weapon_changes.lua
+++ scripts/mods/TourneyBalance/changes/weapon_changes.lua
@@ -398,6 +398,31 @@ mod:add_buff_template("we_deus_01_dot_charged", {
         buff_perks.burning_elven_magic,
     },
 })
+-- Energy recharge rate nerf.
+EnergyData.we_waywatcher = {
+	recharge_delay = 0.2,
+	max_value = 25,
+	depletion_cooldown = 5,
+	recharge_rate = 1 -- 1.5
+}
+EnergyData.we_maidenguard = {
+	recharge_delay = 0.2,
+	max_value = 25,
+	depletion_cooldown = 5,
+	recharge_rate = 1 -- 1.5
+}
+EnergyData.we_shade = {
+	recharge_delay = 0.2,
+	max_value = 25,
+	depletion_cooldown = 5,
+	recharge_rate = 1 -- 1.5
+}
+EnergyData.we_thornsister = {
+	recharge_delay = 0.2,
+	max_value = 25,
+	depletion_cooldown = 5,
+	recharge_rate = 1 -- 1.5
+}
 
 -- Swiftbow
 DamageProfileTemplates.arrow_machinegun.cleave_distribution.attack = 0.25
